03b0e0c973efec19f4e014dc4acf1f9996729aae
unknown <fengbin@ty.cn>
2017-05-17 09:18:33 +0800

    Initial commit

---
+++ ChapterOne/basic.py
@@ -0,0 +1,15 @@
+
+#print "hello world"
+
+#print 10000L
+
+#print repr("hello world")
+
+#print repr(10000L)
+
+print str("hello world")
+
+print str(10000L)
+
+temp=25
+print "The temperature is "+`temp`
