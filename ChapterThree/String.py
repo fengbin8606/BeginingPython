3d23606d02f1ff69a37d83a34edb050792f77583
fengbin8606 <fengbin8606@gmail.com>
2017-07-10 22:22:48 +0800

    charpter three

--- ChapterThree/String.py
+++ ChapterThree/String.py
@@ -90,4 +90,48 @@ print ('%+5d' %10)+'\n'+('%+5d' %-10)
 #字符串方法
 #find方法
 #find方法可以在一个较长的字符串中查找子字符串。返回子字符串在字符串中的最左端索引，没有找到返回-1,这个方法还可以接收可选的起始点和结束点的参数
-subject=''
+subject='$$$ Get rich now !!! $$$'
+print subject.find('$$$')
+print subject.find('$$$',1) #只提供起始点
+print subject.find('!!!')
+print subject.find('!!!',0,16) #提供起始点和结束点 包含第一个索引，但不包含第二个索引
+
+
+#join方法
+#用来在队列中添加元素，需要添加队列的元素必须是字符串，是split方法的逆方法
+seq=[1,2,3,4,5]
+sep='+'
+# print sep.join(seq)
+seq=['1','2','3','4','5']
+print sep.join(seq)
+
+dirs='','usr','bin','env'
+print '/'.join(dirs)
+print 'C:'+'\\'.join(dirs)
+
+#lower方法 返回字符串的小写字母版
+print 'Trondheim Hammer Dance'.lower()
+
+#title方法 将字符串转换成标题 也就是所有单词的首字母大写，其他字母小写
+print "that's all folks".title()
+
+#strip方法 返回去除两侧（不包括内部）空格的字符串
+print '    internal whitespace is kept    '.strip()
+
+#strip方法 也可以指定需要去除的字符，将它们列为参数即可
+print '****SPAM * for * everyone!!!***'.strip(' *!')
+
+#translate方法
+#translate方法和replace方法一样，可以替换字符串中的某些部分，但是只处理单个字符。
+
+#在使用translate转换之前，需要先完成一张转换表，转换表中是以某字符替换某字符的对应关系，一般使用string模块中的maketrans函数
+
+#maketrans函数接收两个参数：两个等长的字符串，表示第一个字符串中的每个字符都用第二个字符串中相同位置的字符替换
+
+from string import maketrans
+table=maketrans('cs','kz')
+print len(table) #这个表有多达256个项目，事实上是字符串
+
+#创建这个表后，可以将它用作translate方法的参数，进行字符串转换
+print 'this is an incredible test'.translate(table)
+
