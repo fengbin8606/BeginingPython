a1573c49b0cebc1c5213c0cad0c229817c4f0557
fengbin <619751941@qq.com>
2017-09-22 17:14:44 +0800

    chapter ten

--- ChapterTen/BatteriesIncluded.py
+++ ChapterTen/BatteriesIncluded.py
@@ -118,3 +118,24 @@ print time.time()
 from random import *
 print random()
 
+
+import re
+some_text = 'alpha, beta,,,,gamma delta'
+print re.split('[, ]+', some_text)
+print re.split('[, ]+', some_text,maxsplit=1)
+print re.split('[, ]+', some_text,maxsplit=2)
+
+print re.split('(0)', 'foobar')
+
+pat = '[a-zA-Z]+'
+text = '"Hm... Err -- are you sure?" he said, sounding insecure.'
+print re.findall(pat, text)
+
+pat = '[.?\-",]+'
+print re.findall(pat,text)
+
+pat = '{name}'
+text = 'Dear {name}...'
+print re.sub(pat, 'Mr. Gumby', text)
+
+print re.escape('www.python.org')
